b5ae7753ab5e43028dad4e3760ccfeb85d921d5c
suraj4502 <sy820860@gmail.com>
2023-06-25 23:58:27 +0530

    prime_nums_inARange

--- numbers/prime_num_in_given_range.py
+++ numbers/prime_num_in_given_range.py
@@ -1,22 +1,13 @@
-'''
-Problem Statement: Given a number, check whether it is prime or not. A prime number is a natural number that is only divisible by 1 and by itself.
 
-Examples 1 2 3 5 7 11 13 17 19 …
 
-Examples
-Example 1:
-Input: N = 3
-Output: Prime
-Explanation: 3 is a prime number
-
-Example 2:
-Input: N = 26
-Output: Non-Prime
-Explanation: 26 is not prime
-'''
-
-from math import sqrt
-
+def isPrime(N):
+    if N <= 1:
+        return False
+    for i in range(2, (N**0.5)+1):
+        if N % i == 0:
+            return False
+        return True
+    
 def isPrime(N):
     # Check if the number is less than or equal to 1
     if N <= 1:
@@ -26,7 +17,7 @@ def isPrime(N):
     # We only need to check divisibility up to the square root of N because
     # if N is divisible by any number greater than its square root, then it
     # would also be divisible by a smaller number.
-    for i in range(2, sqrt(N) + 1):
+    for i in range(2, int(N**0.5) + 1):
         # If N is divisible by i, it is not a prime number
         if N % i == 0:
             return False
@@ -35,10 +26,22 @@ def isPrime(N):
     # then it is a prime number
     return True
 
+
+
+def find_prime_numbers(n):
+    list_of_prime_numbers = [1]
+    
+    for i in range(2,n+1):
+        if isPrime(i):
+            list_of_prime_numbers.append(i)
+            
+    return list_of_prime_numbers
+
+
+
 if __name__ == "__main__":
-    n = int(input("Enter the number :"))
-    ans = isPrime(n)
-    if n != 1 and ans == True:
-        print("Prime Number")
-    else:
-        print("Non Prime Number")
+    n = int(input("Enter the Range in which you want to find the prime numbers : "))
+    print(find_prime_numbers(n))
+
+        
+    
